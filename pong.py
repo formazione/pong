b5c7881b64d1e240632320f448fff9062af29b1c
formazione <gatto.gio@gmail.com>
2020-07-09 20:46:40 +0200

    menu for pong and pause

--- pong.py
+++ pong.py
@@ -85,6 +85,25 @@ def collision():
 pygame.mouse.set_visible(False)
 pygame.event.set_grab(True)
 
+def pause():
+    loop = 1
+    write("PAUSED", 500, 150)
+    write("Press Space to continue", 500, 250)
+    while loop:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                loop = 0
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    loop = 0
+                if event.key == pygame.K_SPACE:
+                    screen.fill((0, 0, 0))
+                    loop = 0
+        pygame.display.update()
+        # screen.fill((0, 0, 0))
+        clock.tick(60)
+
+
 def start():
     loop = 1
     while loop:
@@ -95,6 +114,8 @@ def start():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     loop = 0
+                if event.key == pygame.K_SPACE:
+                    pause()
         gfxdraw.filled_circle(screen, ball.x, ball.y, 5, (0, 0, 0))
         ball.update()
         pygame.draw.rect(screen, BLACK, bar1.rect)
